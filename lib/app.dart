61f87c426f9a28c9bc3d7dad2d9fff0d7714d245
rebecca burch <rebecca.burch.stu@gmail.com>
2020-11-17 20:59:27 -0600

    updated readme, simplified app to just material app

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'database mock testing Demo',
+      theme: ThemeData(
+          // Define the default brightness and colors.
+          ),
+    );
+  }
+}
+
+// 30D4D4
